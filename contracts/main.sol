16458d18fa931ace59c437c9b3732935d65f0a57
scammi <scammi@gmail.com>
2019-01-14 19:08:25 -0300

    added f(addDoctor) & sudo account

--- contracts/main.sol
+++ contracts/main.sol
@@ -2,7 +2,12 @@ pragma solidity ^0.5.0;
 
 contract main{
 
-  address doctor = msg.sender;
+  address goverment;
+  constructor() public{
+    goverment = msg.sender;
+  }
+
+  address[] private doctors;
 
   struct prescription {
     address emmiter;
@@ -13,10 +18,14 @@ contract main{
   uint public prescriptionId = 0;
   mapping (uint => prescription) public prescriptions;
 
-  function addCounter (string memory _medication, uint _dose) public{
+  function addCounter (address _doctor, string memory _medication, uint _dose) public{
     prescriptionId++;
-    prescriptions[prescriptionId] = prescription(doctor, _medication, _dose);
+    prescriptions[prescriptionId] = prescription(_doctor, _medication, _dose);
 
   }
 
+  function addDoctor (address _doctor) public{
+    require(msg.sender == goverment);
+    doctors.push(_doctor);
+  }
 }
