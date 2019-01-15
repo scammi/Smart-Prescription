826cf44b4b5d0b81fde9e6dd75e6cc3a647518cd
scammi <scammi@gmail.com>
2019-01-15 20:50:23 -0300

    documented the code, added check doctor permission to prescribe

--- contracts/main.sol
+++ contracts/main.sol
@@ -4,34 +4,33 @@ contract main{
 
   address goverment;
   uint public prescriptionId;
-  address[] public doctors;
   struct prescription {
     address emmiter;
     string medication;
     uint dose;
   }
+  mapping (uint => prescription) public prescriptions;
+  mapping (address => bool) public doctors;
 
+//owner of the contract has administration privileges
   constructor() public{
     goverment = msg.sender;
   }
-
-  modifier beDoc (address _doctor){
-        require(
-            doctors[_doctor].exists,
-            "Only doctors can call this function."
-        );
-        _;
-    }
-
-  mapping (uint => prescription) public prescriptions;
-
-  function addCounter (address _doctor, string memory _medication, uint _dose) public beDoc(_doctor){
+//government gives permission to prescribe to doctor addresses
+  function addDoctor (address _doctor) public{
+    require(msg.sender == goverment);
+    doctors[_doctor] = true;
+  }
+// checks if doctors have permission to prescribe
+  function seeDoctor (address _doctor) public view returns(bool){
+    return doctors[_doctor];
+  }
+//first checks if doctor allows, if so, it adds a prescription construct to the mapping
+  function addPresciption (address _doctor, string memory _medication, uint _dose) public{
+    require (seeDoctor(_doctor) == true, "ONLY DOCTORS CAN PRESCRIBE");
     prescriptionId++;
     prescriptions[prescriptionId] = prescription(_doctor, _medication, _dose);
   }
 
-  function addDoctor (address _doctor) public{
-    require(msg.sender == goverment);
-    doctors.push(_doctor);
-  }
+
 }
